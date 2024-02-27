b43b78b30c86faba784d72ecb9a5b9ff6a82c0b3
Sonia Pires <157916578+soniathephoenix@users.noreply.github.com>
2024-02-27 12:11:13 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+let countElh = document.getElementById("count-elh")
+let countElg = document.getElementById("count-elg")
+let count = 0
+
+function add1h() {
+    count += 1
+    countElh.textContent = count
+}
+
+function add2h() {
+    count += 2
+    countElh.textContent = count
+}
+
+function add3h() {
+    count += 3
+    countElh.textContent = count
+}
+
+
+function add1g() {
+    count += 1
+    countElg.textContent = count
+}
+
+function add2g() {
+    count += 2
+    countElg.textContent = count
+}
+
+function add3g() {
+    count += 3
+    countElg.textContent = count
+}
